74b0120d1ec012c0f74e0f77a841989820efa0a6
Peter Seibel <peter@gigamonkeys.com>
2026-08-05 13:43:35 -0700

    Updating to JDK 26 javadoc links.

--- fix-javadocs.pl
+++ fix-javadocs.pl
@@ -3,8 +3,11 @@
 use warnings;
 use strict;
 
+my $JDK_VERSION = 26;
+
 while (<>) {
-    s{"https?://docs.oracle.com/javase/\d+/(docs/api)/(.*?)"}{"https://docs.oracle.com/en/java/javase/22/$1/java.base/$2"}g;
+    s{"https?://docs.oracle.com/javase/\d+/(docs/api)/(.*?)"}{"https://docs.oracle.com/en/java/javase/$JDK_VERSION/$1/java.base/$2"}g;
+    s{"https://docs.oracle.com/en/java/javase/\d+/docs/api/java\.(.*?)/(.*?)"}{"https://docs.oracle.com/en/java/javase/$JDK_VERSION/docs/api/java.$1/$2"}g;
     print;
 }
 
